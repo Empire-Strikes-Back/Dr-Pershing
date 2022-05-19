3f7610e4eff6b53bbc158860ec0b554db206a8e2
Dr-Pershing <Dr-Pershing@i-i-protected-him>
2022-05-19 19:35:10 +0300

    i am terminal .jar program with Madison nodejs subprocess for ipfs netwroking :Graham-Norton didn't they say - let's try one more time? :Benedict-Cumberbatch m m maaybee

--- program.sh
+++ program.sh
@@ -2,7 +2,7 @@
 
 repl(){
   clj \
-    -J-Dclojure.core.async.pool-size=8 \
+    -J-Dclojure.core.async.pool-size=1 \
     -X:Ripley Ripley.core/process \
     :main-ns Dr-Pershing.main
 }
@@ -17,8 +17,10 @@ main(){
 tag(){
   COMMIT_HASH=$(git rev-parse --short HEAD)
   COMMIT_COUNT=$(git rev-list --count HEAD)
-  git tag "$COMMIT_COUNT-$COMMIT_HASH" $COMMIT_HASH
-  git tag -l
+  TAG="$COMMIT_COUNT-$COMMIT_HASH"
+  git tag $TAG $COMMIT_HASH
+  echo $COMMIT_HASH
+  echo $TAG
 }
 
 jar(){
@@ -30,10 +32,35 @@ jar(){
     -X:Genie Genie.core/process \
     :main-ns Dr-Pershing.main \
     :filename "\"out/Dr-Pershing-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
-    :paths '["src"]'
+    :paths '["src" "out/ui" "out/corn"]'
+}
+
+Madison_install(){
+  npm i --no-package-lock
+  mkdir -p out/ui/
+  cp src/Dr_Pershing/index.html out/ui/index.html
+  cp src/Dr_Pershing/style.css out/ui/style.css
+  mkdir -p out/corn/
+  cp package.json out/corn/package.json
+}
+
+Moana(){
+  clj -A:Moana:ui:corn -M -m shadow.cljs.devtools.cli "$@"
+}
+
+Madison_repl(){
+  Madison_install
+  Moana clj-repl
+  # (shadow/watch :main)
+  # (shadow/watch :ui)
+  # (shadow/repl :main)
+  # :repl/quit
 }
 
 release(){
+  rm -rf out
+  Madison_install
+  Moana release :ui :corn
   jar
 }
 
--- src/Dr_Pershing/main.clj
+++ src/Dr_Pershing/main.clj
@@ -6,32 +6,94 @@
             go >! <! alt! alts! do-alts
             mult tap untap pub sub unsub mix unmix admix
             pipe pipeline pipeline-async]]
+   [clojure.core.async.impl.protocols :refer [closed?]]
    [clojure.java.io :as Wichita.java.io]
    [clojure.string :as Wichita.string]
    [clojure.pprint :as Wichita.pprint]
    [clojure.repl :as Wichita.repl]
+   [clojure.java.shell :as Wichita.java.shell]
+
+   [aleph.http :as Simba.http]
 
    [Dr-Pershing.seed]
-   [Dr-Pershing.sunflower_seeds]
+   [Dr-Pershing.microwaved-turnips]
    [Dr-Pershing.radish]
-
-   [Ripley.core])
+   [Dr-Pershing.beans])
+  (:import
+   (java.io File))
   (:gen-class))
 
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
 (defonce stateA (atom nil))
+(defonce host| (chan 1))
+(defonce corn| (chan (sliding-buffer 10)))
 
 (defn reload
   []
   (require
    '[Dr-Pershing.seed]
+   '[Dr-Pershing.microwaved-turnips]
+   '[Dr-Pershing.beans]
    '[Dr-Pershing.main]
    :reload))
 
 (defn -main
   [& args]
-  #_(println "i dont want my next job")
   (println "if it wansn't for me he would already be dead - please! please!")
-  #_(println "Kuiil has spoken")
-  (Ripley.core/process {:main-ns 'Dr-Pershing.main}))
+  (println "i dont want my next job")
+  (println "Kuiil has spoken")
+
+  (let [data-dir-path (or
+                       (some-> (System/getenv "DR_PERSHING_PATH")
+                               (.replaceFirst "^~" (System/getProperty "user.home")))
+                       (.getCanonicalPath ^File (Wichita.java.io/file (System/getProperty "user.home") ".Dr-Pershing")))
+        state-file-path (.getCanonicalPath ^File (Wichita.java.io/file data-dir-path "Dr-Pershing.edn"))]
+    (Wichita.java.io/make-parents data-dir-path)
+    (reset! stateA {})
+
+
+    (remove-watch stateA :watch-fn)
+    (add-watch stateA :watch-fn
+               (fn [ref wathc-key old-state new-state]
+
+                 (when (not= old-state new-state))))
+
+
+    (let [port (or (try (Integer/parseInt (System/getenv "PORT"))
+                        (catch Exception e nil))
+                   3366)]
+      (Dr-Pershing.microwaved-turnips/process
+       {:port port
+        :host| host|
+        :corn| corn|}))
+
+    (let [path-db (.getCanonicalPath ^File (Wichita.java.io/file data-dir-path "Deep-Thought"))]
+      (Wichita.java.io/make-parents path-db)
+      (Dr-Pershing.beans/process {:path path-db}))
+
+    (go
+      (let []
+        (doseq [filepath ["corn.js" "package.json"]]
+          (->
+           (Wichita.java.io/file (Wichita.java.io/resource filepath))
+           (Wichita.java.io/copy (Wichita.java.io/file (.getCanonicalPath ^File (Wichita.java.io/file data-dir-path filepath)))))))
+
+      (do
+        (->
+         (ProcessBuilder. ["npm" "install" "--force"])
+         (.directory (Wichita.java.io/file data-dir-path))
+         (.inheritIO)
+         (.start)
+         (.waitFor))
+        (->
+         (ProcessBuilder. ["node" "corn.js"])
+         (.directory (Wichita.java.io/file data-dir-path))
+         (.inheritIO)
+         (.start))))
+
+    (go
+      (loop []
+        (when-let [value (<! corn|)]
+          (println :corn-message value)
+          (recur))))))
